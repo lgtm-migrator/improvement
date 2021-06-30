9ca3bf22a5ffcdfc895ea47b5a316f45f39ab609
Henri Kestiö <henri.kestio@gmail.com>
2021-06-30 15:52:00 +0300

    Change state management from recoil to redux

--- src/tests/Signup.test.tsx
+++ src/tests/Signup.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+import store from '../state/store'
 import Signup from '../pages/Signup'
 
 it('should render sign up page', () => {
-    const { queryByTitle } = render(<Signup isAuthenticated={false} />)
+    const { queryByTitle } = render(<Provider store={store}><Signup isAuthenticated={false} /></Provider>)
     const usernameInput = queryByTitle('usernameInput')
     const passwordInput = queryByTitle('passwordInput')
     const signUpBtn = queryByTitle('signupBtn')
